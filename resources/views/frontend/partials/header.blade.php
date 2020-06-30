5ba2e5506e4872bdd3d287e2dfb2c059decfbc36
Nguyễn Văn Trọng <vantrong@toplearning.vn>
2020-06-30 21:51:28 +0700

    update frontend

---
+++ resources/views/frontend/partials/header.blade.php
@@ -0,0 +1,51 @@
+<header id="header">
+    <div class="container">
+        <div class="row" id="headwrap">
+            <div class="col-md-3 col-sm-6 slogan">
+                <h1 class="site-title"><a class="logo" href="/" rel="home">XemPhimPlus | Phim Hay | Phim Mới</a></h1>
+            </div>
+
+            <div class="col-md-5 col-sm-6 halim-search-form hidden-xs">
+                <div class="header-nav">
+                    <div class="col-xs-12">
+                        <form id="search-form-pc" name="halimForm" role="search" action="/" method="GET">
+                            <div class="form-group">
+                                <div class="input-group col-xs-12">
+                                    <input id="search" type="text" name="s" value="" class="form-control" data-toggle="tooltip" data-placement="bottom" data-original-title="Nhấn Enter để tìm kiếm" placeholder="Tìm kiếm với 3,119 bộ phim..." autocomplete="off" required>
+                                    <i class="animate-spin hl-spin4 hidden"></i>
+                                </div>
+                            </div>
+                        </form>
+                        <ul class="ui-autocomplete ajax-results hidden"></ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 hidden-xs">
+                <div id="get-bookmark" class="box-shadow"><i class="hl-bookmark"></i><span> Tủ phim</span><span class="count">0</span></div>
+                <div class="user user-login-option box-shadow" id="pc-user-login">
+                    <div class="dropdown">
+                        <a href="javascript:;" class="avt" id="userInfo" onclick="openLoginModal();">
+                            <img alt='' src='http://1.gravatar.com/avatar/?s=20&#038;d=mm&#038;r=g' srcset='http://2.gravatar.com/avatar/?s=40&#038;d=mm&#038;r=g 2x' class='avatar avatar-20 photo avatar-default' height='20' width='20' />                            <span class="name">Đăng nhập</span>
+                        </a>
+                        <ul class="dropdown-menu login-box" aria-labelledby="userInfo">
+                            <li class="twitter box-shadow">
+                                <a rel="nofollow" href="/wp-login.php?apsl_login_id=twitter_login&state=cmVkaXJlY3RfdG89aHR0cCUzQSUyRiUyRnhlbXBoaW1wbHVzLm5ldCUyRg==" title='Đăng nhập bằng twitter'><i class="hl-twitter"></i> Login with <span>twitter</span>
+                                </a>
+                            </li>
+                            <li class="facebook box-shadow">
+                                <a rel="nofollow" href="/wp-login.php?apsl_login_id=facebook_login&state=cmVkaXJlY3RfdG89aHR0cCUzQSUyRiUyRnhlbXBoaW1wbHVzLm5ldCUyRg==" title='Đăng nhập bằng facebook'><i class="hl-facebook"></i> Login with <span>facebook</span>
+                                </a>
+                            </li>
+                            <li class="google box-shadow">
+                                <a rel="nofollow" href="/wp-login.php?apsl_login_id=google_login&state=cmVkaXJlY3RfdG89aHR0cCUzQSUyRiUyRnhlbXBoaW1wbHVzLm5ldCUyRg==" title='Đăng nhập bằng google'><i class="hl-gplus"></i> Login with <span>google</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="bookmark-list" class="hidden bookmark-list-on-pc"><ul style="margin: 0;"></ul></div>
+            </div>
+
+        </div>
+    </div>
+</header>
---
+++ resources/views/frontend/partials/slider.blade.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="text-center"></div>    <div class="row fullwith-slider">
+        <!-- Wrapper For Slides -->
+        <div id="halim-fullwith-slider-widget-2" class="owl-carousel owl-carousel-fullwidth owl-theme">
+            @for ($i = 0; $i < 4; $i++)
+                <div class="post-{{ $i }} item">
+                    <a href="/hoa-bi-ngan-beautiful-reborn-flower" title="Hoa Bỉ Ngạn">
+                        <img src="{{ asset('uploads/2020/05/binganhoa.gif') }}" alt="Hoa Bỉ Ngạn"  class="slide-image" />
+                        <div class="slide-text">
+                            <h3 class="slider-title">Hoa Bỉ Ngạn</h3>
+                            <div class="slider-meta hidden-xs">
+                                <p>[Beautiful Reborn Flower (2020)]</p>										</div>
+                        </div>
+                    </a>
+                </div>
+            @endfor
+        </div><!-- End of Wrapper For Slides -->
+        <script>
+            jQuery(document).ready(function($) {
+                var owl = $('#halim-fullwith-slider-widget-2');
+                owl.owlCarousel({
+                    rtl:false,
+                    items: 1,
+                    loop: true,
+                    animateOut: 'fadeOutLeft',
+                    animateIn: 'fadeInRight',
+                    smartSpeed:450,
+                    autoplay: true,
+                    autoplayTimeout: 4000,
+                    autoHeight: true,
+                    autoplayHoverPause: true,
+                    nav: true,
+                    navText: ['<i class="hl-down-open rotate-left"></i>', '<i class="hl-down-open rotate-right"></i>'],
+                    responsiveClass: true,
+                });
+            });
+        </script>
+    </div>
+</div>
