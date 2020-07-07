032d2c920c9bbf36f7b3743cbca2ca92406c0e0a
Đặng Thế Anh <dangtheanh16@gmail.com>
2020-07-07 21:45:07 +0700

    video files

---
+++ app/Models/VideoFiles.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\VideoFiles
+ *
+ * @property int $id
+ * @property int $server_id
+ * @property int $movie_id
+ * @property string $label
+ * @property int $order
+ * @property string $source
+ * @property string $url
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles whereLabel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles whereMovieId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles whereOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles whereServerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles whereSource($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\VideoFiles whereUrl($value)
+ * @mixin \Eloquent
+ */
+class VideoFiles extends Model
+{
+    protected $table = 'video_files';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'label',
+        'order',
+        'source',
+        'url',
+    ];
+}
---
+++ database/migrations/2020_07_07_144026_create_video_files_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoFilesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('video_files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('server_id')->index();
+            $table->bigInteger('movie_id')->index();
+            $table->string('label');
+            $table->integer('order')->default(1);
+            $table->string('source', 100);
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('video_files');
+    }
+}
