fe1a224eac030972a5c685a262166138de16953f
Đặng Thế Anh <dangtheanh16@gmail.com>
2021-05-23 10:16:31 +0700

    :truck: Modules

--- modules/LiveTv/resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('livetv::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('livetv.name') !!}
-    </p>
-@endsection
