290253cb7a417a936e18064a25d7c4307f9ea25e
Đặng Thế Anh <dangtheanh16@gmail.com>
2021-05-29 17:17:01 +0700

    :bug: Fix ip2location

--- mymo-core/Core/Helpers/IP2Location.php
+++ mymo-core/Core/Helpers/IP2Location.php
@@ -1,41 +1,35 @@
 <?php
+/**
+ * MYMO CMS - Free Laravel CMS
+ *
+ * @package    mymocms/mymocms
+ * @author     The Anh Dang <dangtheanh16@gmail.com>
+ * @link       https://github.com/mymocms/mymocms
+ * @license    MIT
+ *
+ * Created by The Anh.
+ * Date: 5/25/2021
+ * Time: 10:05 PM
+ */
 
 namespace Mymo\Core\Helpers;
 
 class IP2Location
 {
-    private function load($mode)
+    private function load()
     {
-        if ($mode == 'bin')
-        {
-            $this->db = new \IP2Location\Database($this->getDatabasePath(), \IP2Location\Database::FILE_IO);
-        } else if ($mode == 'ws')
-        {
-            $apikey = \Config::get('site_vars.IP2LocationAPIKey');
-            $package = (null !== \Config::get('site_vars.IP2LocationPackage')) ? \Config::get('site_vars.IP2LocationPackage') : 'WS1';
-            $ssl = (null !== \Config::get('site_vars.IP2LocationUsessl')) ? \Config::get('site_vars.IP2LocationUsessl') : false;
-            $this->ws = new \IP2Location\WebService($apikey, $package, $ssl);
-        }
+        $this->db = new \IP2Location\Database($this->getDatabasePath(), \IP2Location\Database::FILE_IO);
     }
 
     private function getDatabasePath()
     {
-        return config('ip2locationlaravel.ip2location.local.path');
+        return core_path('database/iplocation/IPV6-COUNTRY.BIN');
     }
 
     public function get($ip, $mode = 'bin')
     {
-        $this->load($mode);
-        if ($mode == 'bin')
-        {
-            $records = $this->db->lookup($ip, \IP2Location\Database::ALL);
-        } else if ($mode == 'ws')
-        {
-            $addons = (null !== \Config::get('site_vars.IP2LocationAddons')) ? \Config::get('site_vars.IP2LocationAddons') : [];
-            $language = (null !== \Config::get('site_vars.IP2LocationLanguage')) ? \Config::get('site_vars.IP2LocationLanguage') : 'en';
-            $records = $this->ws->lookup($ip, $addons, $language);
-        }
-        
+        $this->load();
+        $records = $this->db->lookup($ip, \IP2Location\Database::ALL);
         return $records;
     }
 }
--- mymo-core/Core/Providers/IP2LocationServiceProvider.php
+++ mymo-core/Core/Providers/IP2LocationServiceProvider.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * MYMO CMS - Free Laravel CMS
+ *
+ * @package    mymocms/mymocms
+ * @author     The Anh Dang <dangtheanh16@gmail.com>
+ * @link       https://github.com/mymocms/mymocms
+ * @license    MIT
+ *
+ * Created by The Anh.
+ * Date: 5/25/2021
+ * Time: 10:05 PM
+ */
 
 namespace Mymo\Core\Providers;
 
@@ -7,23 +19,8 @@ use Mymo\Core\Helpers\IP2Location;
 
 class IP2LocationServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->app->bind('ip2location', IP2Location::class);
-    }
-
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
     }
 }
