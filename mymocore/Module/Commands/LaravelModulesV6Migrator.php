5710f754cf21f56de9d7a735306e308c100610af
mymocms <dangtheanh16@gmail.com>
2021-06-05 17:54:26 +0700

    :bug: Rename config plugin

--- mymocore/Module/Commands/LaravelModulesV6Migrator.php
+++ mymocore/Module/Commands/LaravelModulesV6Migrator.php
@@ -11,7 +11,7 @@ use Mymo\Module\Module;
 class LaravelModulesV6Migrator extends Command
 {
     protected $name = 'plugin:v6:migrate';
-    protected $description = 'Migrate laravel-modules v5 modules statuses to v6.';
+    protected $description = 'Migrate plugins v5 modules statuses to v6.';
 
     public function handle() : int
     {
