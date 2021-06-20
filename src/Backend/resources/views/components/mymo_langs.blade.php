e397e77f02b5a4755a3dba730cd0cc42ebf07bcb
mymocms <dangtheanh16@gmail.com>
2021-06-20 18:43:27 +0700

    :bug: Fix mymo langs

--- src/Backend/resources/views/components/mymo_langs.blade.php
+++ src/Backend/resources/views/components/mymo_langs.blade.php
@@ -1,15 +1,10 @@
 <script type="text/javascript">
     /**
-     * MYMO CMS - Free Laravel CMS
+     * MYMO CMS - THE BEST LARAVEL CMS
      *
      * @package    mymocms/mymocms
-     * @author     The Anh Dang <dangtheanh16@gmail.com>
      * @link       https://github.com/mymocms/mymocms
      * @license    MIT
-     *
-     * Created by The Anh.
-     * Date: 5/30/2021
-     * Time: 11:01 AM
      */
 
     var mymo = {
