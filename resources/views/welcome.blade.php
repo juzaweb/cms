de176d32b684f674b04ded373929bb38d19c954d
Juzaweb <dangtheanh16@gmail.com>
2021-08-14 19:53:36 +0700

    :sparkles: Add view default

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * JUZAWEB CMS - The Best CMS for Laravel Project
+ *
+ * @package    juzaweb/laravel-cms
+ * @link       https://juzaweb.com/cms
+ * @license    MIT
+ *
+ * Created by JUZAWEB.
+ * Date: 8/14/2021
+ * Time: 7:32 PM
+ */
+
