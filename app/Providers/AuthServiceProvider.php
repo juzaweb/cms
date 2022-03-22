dbb45ef98dcbd52c37c70421f44d0220fe7adfb6
Juzaweb <dangtheanh16@gmail.com>
2022-03-22 20:35:23 +0700

    feat/api :sparkles: Breeze auth

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+    
+        Gate::before(
+            function ($user, $ability) {
+                if ($user->isAdmin()) {
+                    return true;
+                }
+            
+                return null;
+            }
+        );
+    
+        ResetPassword::createUrlUsing(function ($notifiable, $token) {
+            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+    }
+}
