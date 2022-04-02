ab73edd94e0e275d43427b125fd43341df676569
Juzaweb <dangtheanh16@gmail.com>
2022-04-02 15:53:31 +0700

    fix/fix-theme-route :bug: Fix theme routes

--- packages/backend/src/routes/components/dashboard.route.php
+++ packages/backend/src/routes/components/dashboard.route.php
@@ -8,26 +8,40 @@
  * @license    MIT
  */
 
-Route::group(['prefix' => '/'], function () {
-    Route::get('/', 'Backend\DashboardController@index')->name('admin.dashboard');
-
-    Route::get('/load-data/{func}', 'Backend\LoadDataController@loadData')->name('admin.load_data');
-
-    Route::get('/dashboard/users', 'Backend\DashboardController@getDataUser')->name('admin.dashboard.users');
-
-    Route::get(
-        '/dashboard/top-views',
-        'Backend\DashboardController@getDataTopViews'
-    )->name('admin.dashboard.top_views');
-
-    Route::get('/dashboard/views-chart', 'Backend\DashboardController@viewsChart')->name('admin.dashboard.views_chart');
-
-    Route::get('/datatable/get-data', 'Backend\DatatableController@getData')->name('admin.datatable.get-data');
-
-    Route::post(
-        '/datatable/bulk-actions',
-        'Backend\DatatableController@bulkActions'
-    )->name('admin.datatable.bulk-actions');
-});
+Route::group(
+    ['prefix' => '/'],
+    function () {
+        Route::get('/', 'Backend\DashboardController@index')->name('admin.dashboard');
+    
+        Route::get('/load-data/{func}', 'Backend\LoadDataController@loadData')->name('admin.load_data');
+    
+        Route::get(
+            '/dashboard/users',
+            'Backend\DashboardController@getDataUser'
+        )
+            ->name('admin.dashboard.users');
+    
+        Route::get(
+            '/dashboard/top-views',
+            'Backend\DashboardController@getDataTopViews'
+        )->name('admin.dashboard.top_views');
+    
+        Route::get(
+            '/dashboard/views-chart',
+            'Backend\DashboardController@viewsChart'
+        )->name('admin.dashboard.views_chart');
+    
+        Route::get(
+            '/datatable/get-data',
+            'Backend\DatatableController@getData'
+        )
+            ->name('admin.datatable.get-data');
+    
+        Route::post(
+            '/datatable/bulk-actions',
+            'Backend\DatatableController@bulkActions'
+        )->name('admin.datatable.bulk-actions');
+    }
+);
 
 Route::any('/ajax/{slug}', 'Backend\AjaxController@handle')->name('admin.ajax');
