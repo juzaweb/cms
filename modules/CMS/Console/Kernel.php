19fd5a4a461bba800e68027d9b2750900b243a41
Juzaweb <dangtheanh16@gmail.com>
2022-04-06 22:11:56 +0700

    :truck: Add module cms

--- modules/CMS/Console/Kernel.php
+++ modules/CMS/Console/Kernel.php
@@ -25,8 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        // $this->load(__DIR__.'/Commands');
         
-        require base_path('routes/console.php');
+        // require base_path('routes/console.php');
     }
 }
