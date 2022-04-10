0ccd1685e3604fec2b3ee114d69d1cb993e109a2
Juzaweb <dangtheanh16@gmail.com>
2022-04-10 13:39:47 +0700

    :sparkles: Add none mix assets cms

--- modules/Backend/resources/assets/plugins/bootstrap/js/bootstrap.bundle.js
+++ modules/Backend/resources/assets/plugins/bootstrap/js/bootstrap.bundle.js
@@ -1,4 +1,4 @@
-/*!
+/*
   * Bootstrap v4.4.1 (https://getbootstrap.com/)
   * Copyright 2011-2019 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,19 @@
 const mix = require('laravel-mix');
 const modulePath = `${__dirname}/modules/Backend/resources/assets`;
 
+mix.disableNotifications();
+mix.options({
+    postCss: [
+        require('postcss-discard-comments') (
+            {
+                removeAll: true
+            }
+        )
+    ],
+    uglify: {
+        comments: false
+    }
+});
+
 //require(`${modulePath}/mix.js`);
-//require(`${modulePath}/filemanage.mix.js`);
+require(`${modulePath}/filemanage.mix.js`);
