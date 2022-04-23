c3cca1aa72b7c6e0b06355b67dc6c7ffe067b0ad
Juzaweb <dangtheanh16@gmail.com>
2022-04-23 19:11:26 +0700

    :tada: Add example plugin

---
+++ plugins/example/src/Providers/ExampleServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Juzaweb\Example\Providers;
+
+use Juzaweb\CMS\Support\ServiceProvider;
+
+class ExampleServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
