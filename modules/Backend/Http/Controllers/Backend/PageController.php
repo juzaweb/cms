64e72ecef8fbb26799031aeb1367862a11e2aa0f
Juzaweb <dangtheanh16@gmail.com>
2022-04-24 12:52:08 +0700

    :bug: Fix register admin page

--- modules/Backend/Http/Controllers/Backend/PageController.php
+++ modules/Backend/Http/Controllers/Backend/PageController.php
@@ -9,15 +9,15 @@ use Juzaweb\Backend\Facades\HookAction;
 
 class PageController extends BackendController
 {
-    protected $page;
-    
+    protected Collection $page;
+
     public function callAction($method, $parameters)
     {
         $this->page = $this->findPageOrFail();
-    
+
         return parent::callAction($method, $parameters);
     }
-    
+
     /**
      * @throws \Symfony\Component\HttpKernel\Exception\HttpException
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
@@ -25,17 +25,18 @@ class PageController extends BackendController
     protected function findPageOrFail() : Collection
     {
         $page = HookAction::getAdminPages($this->getPageSlug());
+
         if (empty($page)) {
             abort(404);
         }
-        
+
         if (is_array($page)) {
             abort(404);
         }
-        
+
         return $page;
     }
-    
+
     protected function getPageSlug()
     {
         $slugs = explode('/', Route::getCurrentRoute()->uri);
