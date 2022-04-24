4e881c1bab206c22b482ffb83255c0f6ba07df3c
Juzaweb <dangtheanh16@gmail.com>
2022-04-24 16:00:16 +0700

    :bug: Fix enqueue styles

---
+++ modules/Frontend/resources/views/frontend/styles.blade.php
@@ -0,0 +1,14 @@
+@foreach ($styles as $style)
+    @php
+        $href = $style->get('src') .'?v='. $style->get('ver');
+    @endphp
+    <link rel="stylesheet" type="text/css" href="{{ $href }}" id="{{ $style->get('key') }}">
+@endforeach
+
+@foreach ($scripts as $script)
+    @php
+        $href = $script->get('src') .'?v='. $script->get('ver');
+    @endphp
+
+    <script src="{{ $href }}" id="{{ $script->get('key') }}"></script>
+@endforeach
