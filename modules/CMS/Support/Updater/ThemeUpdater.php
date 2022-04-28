686aef1be0360156107fd360de7df633fc1cef95
Juzaweb <dangtheanh16@gmail.com>
2022-04-28 22:05:54 +0700

    :sparkles: Install theme

--- modules/CMS/Support/Updater/ThemeUpdater.php
+++ modules/CMS/Support/Updater/ThemeUpdater.php
@@ -70,6 +70,6 @@ class ThemeUpdater extends UpdateManager
 
     protected function getLocalPath(): string
     {
-        return config('juzaweb.theme.path').$this->name;
+        return config('juzaweb.theme.path').'/'.$this->name;
     }
 }
