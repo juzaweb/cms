a4612e7742867dff36f19d6713a3796b06251f12
Juzaweb <dangtheanh16@gmail.com>
2022-04-28 23:01:04 +0700

    :sparkles: Add style publish themes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 const mix = require('laravel-mix');
 const modulePath = `${__dirname}/modules/Backend/resources/assets`;
 const pluginPath = `${__dirname}/plugins`;
+const themePath = `${__dirname}/themes`;
 
 mix.disableNotifications();
 mix.options({
@@ -16,6 +17,7 @@ mix.options({
     }
 });
 
-require(`${modulePath}/mix.js`);
+//require(`${modulePath}/mix.js`);
 //require(`${modulePath}/filemanage.mix.js`);
 //require(`${pluginPath}/ecommerce/src/resources/assets/mix.js`);
+require(`${themePath}/gamxo/assets/mix.js`);
