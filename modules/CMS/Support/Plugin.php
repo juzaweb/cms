03ffc75c64fa393952ee82b45992d334f6d5b5f7
Juzaweb <dangtheanh16@gmail.com>
2022-05-08 14:56:02 +0700

    :+1: Custom autoload file plugins

--- modules/CMS/Support/Plugin.php
+++ modules/CMS/Support/Plugin.php
@@ -349,7 +349,7 @@ class Plugin
             ))
                 ->load($providers);
         } catch (\Throwable $e) {
-            $this->disable();
+            //$this->disable();
             throw $e;
         }
     }
