202364d40620fb3a6ef0fe1aa92fa48df47dfce4
Juzaweb <dangtheanh16@gmail.com>
2022-05-14 13:22:29 +0700

    :truck: Theme helpers

---
+++ modules/CMS/Facades/Theme.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * JUZAWEB CMS - The Best CMS for Laravel Project
+ *
+ * @package    juzaweb/juzacms
+ * @author     The Anh Dang <dangtheanh16@gmail.com>
+ * @link       https://juzaweb.com/cms
+ * @license    MIT
+ */
+
+namespace Juzaweb\CMS\Facades;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Facade as FacadeAlias;
+use Juzaweb\CMS\Support\LocalThemeRepository;
+
+/**
+ * @method static Theme|null find(string $name)
+ * @method static array|Collection all(bool $collection = false)
+ *
+ * @see LocalThemeRepository
+ */
+class Theme extends FacadeAlias
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'themes';
+    }
+}
