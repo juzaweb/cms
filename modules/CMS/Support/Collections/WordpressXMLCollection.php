d3ef26f18a9cfb045fef3f2b019a841f649de34d
Juzaweb <47020363+juzaweb@users.noreply.github.com>
2022-05-23 19:29:13 +0700

    :construction: Import from wordpress

--- modules/CMS/Support/Collections/WordpressXMLCollection.php
+++ modules/CMS/Support/Collections/WordpressXMLCollection.php
@@ -29,21 +29,43 @@ class WordpressXMLCollection implements XMLCollectionInterface
     protected function collectItemData(\SimpleXMLElement $item): array
     {
         foreach ($item->category as $category) {
-            $data[$this->parstTaxonomy((string) $category['domain'])][] = [
+            $data[$this->parseTaxonomy((string) $category['domain'])][] = [
                 'name' => (string) $category,
                 'slug' => (string) $category['nicename'],
             ];
         }
 
         $data['title'] = (string) $item->title;
-        $data['type'] = (string) $item->post_type;
-        $data['slug'] = (string) $item->guid;
-        $data['content'] = (string) $item->children('http://purl.org/rss/1.0/modules/content/')->encoded;
-
+        $data['type'] = Str::plural((string) $this->getExportData($item, 'post_type'));
+        $data['slug'] = (string) $this->getExportData($item, 'post_name');
+        $data['content'] = $this->getContent($item);
+        $data['metas'] = $this->getMetas($item);
         return $data;
     }
 
-    protected function parstTaxonomy(string $taxonomy): string
+    protected function getMetas(\SimpleXMLElement $item): array
+    {
+        $metas = [];
+        $data = $this->getExportData($item, 'postmeta');
+        foreach ($data as $item) {
+            $metas[(string) $item->meta_key] = (string) $item->meta_value;
+        }
+        return $metas;
+    }
+
+    protected function getExportData(\SimpleXMLElement $item, string $name): ?\SimpleXMLElement
+    {
+        return $item->children('http://wordpress.org/export/1.2/')->{$name};
+    }
+
+    protected function getContent(\SimpleXMLElement $item): string
+    {
+        $content = (string) $item->children('http://purl.org/rss/1.0/modules/content/')->encoded;
+
+        return $content;
+    }
+
+    protected function parseTaxonomy(string $taxonomy): string
     {
         if ($taxonomy == 'post_tag') {
             $taxonomy = 'tag';
