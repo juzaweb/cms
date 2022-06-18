b9c745652a0b7f1d3d4813edaad694de5963a01f
Juzaweb <dangtheanh16@gmail.com>
2022-06-18 12:07:31 +0700

    :+1: Add proxy url for dev

--- modules/CMS/Database/migrations/v1.0.5/2020_06_17_141314_create_posts_table.php
+++ modules/CMS/Database/migrations/v1.0.5/2020_06_17_141314_create_posts_table.php
@@ -1,5 +1,4 @@
 <?php
-
 // phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
 
 use Illuminate\Support\Facades\Schema;
@@ -10,30 +9,36 @@ class CreatePostsTable extends Migration
 {
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('title', 250);
-            $table->string('thumbnail', 250)->nullable();
-            $table->string('slug', 150)->unique();
-            $table->string('description', 200)->nullable();
-            $table->longText('content')->nullable();
-            $table->string('status', 50)->index()->default('draft');
-            $table->bigInteger('views')->default(0);
-            $table->timestamps();
-        });
+        Schema::create(
+            'posts',
+            function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('title', 250);
+                $table->string('thumbnail', 250)->nullable();
+                $table->string('slug', 150)->unique();
+                $table->string('description', 200)->nullable();
+                $table->longText('content')->nullable();
+                $table->string('status', 50)->index()->default('draft');
+                $table->bigInteger('views')->default(0);
+                $table->timestamps();
+            }
+        );
 
-        Schema::create('post_metas', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('post_id')->index();
-            $table->string('meta_key', 150)->index();
-            $table->text('meta_value')->nullable();
-            $table->unique(['post_id', 'meta_key']);
+        Schema::create(
+            'post_metas',
+            function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->unsignedBigInteger('post_id')->index();
+                $table->string('meta_key', 150)->index();
+                $table->text('meta_value')->nullable();
+                $table->unique(['post_id', 'meta_key']);
 
-            $table->foreign('post_id')
-                ->references('id')
-                ->on('posts')
-                ->onDelete('cascade');
-        });
+                $table->foreign('post_id')
+                    ->references('id')
+                    ->on('posts')
+                    ->onDelete('cascade');
+            }
+        );
     }
 
     public function down()
