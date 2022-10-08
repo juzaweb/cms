43e544f1c768b5c35ec6bd73895b942ba4f5d89c
Juzaweb <dangtheanh16@gmail.com>
2022-10-08 13:58:53 +0700

    :+1: Buy theme

--- modules/Backend/resources/views/backend/theme/install.blade.php
+++ modules/Backend/resources/views/backend/theme/install.blade.php
@@ -30,28 +30,6 @@
 
     <div class="row" id="theme-list"></div>
 
-    <template id="theme-template">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="height-200 d-flex flex-column jw__g13__head" style="background-image: url('{screenshot}')">
-                </div>
-
-                <div class="card card-borderless mb-0">
-                    <div class="card-header border-bottom-0">
-                        <div class="d-flex">
-                            <div class="text-dark text-uppercase font-weight-bold mr-auto">
-                                {title}
-                            </div>
-                            <div class="text-gray-6">
-                                <button class="btn btn-primary install-theme" data-theme="{name}"> {{ trans('cms::app.install') }}</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </template>
-
     <script>
         function themeItemFormatter(index, row)
         {
@@ -87,7 +65,6 @@
         var listView = new JuzawebListView({
             url: "{{ route('admin.theme.install.all') }}",
             list: "#theme-list",
-            template: "theme-template",
             page_size: 9,
             item_formatter: "themeItemFormatter"
         });
--- modules/CMS/Support/Updater/ThemeUpdater.php
+++ modules/CMS/Support/Updater/ThemeUpdater.php
@@ -3,6 +3,7 @@
 namespace Juzaweb\CMS\Support\Updater;
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
 use Juzaweb\CMS\Abstracts\UpdateManager;
 use Juzaweb\CMS\Facades\CacheGroup;
 use Juzaweb\CMS\Version;
@@ -64,15 +65,29 @@ class ThemeUpdater extends UpdateManager
 
         $response = $this->api->get(
             $uri,
-            [
-                'current_version' => $this->getCurrentVersion(),
-                'cms_version' => Version::getVersion()
-            ]
+            $this->getDefaultApiParams()
         );
 
         $this->responseErrors($response);
 
         return $response;
+    }
+
+    protected function getDefaultApiParams(): array
+    {
+        $params = [
+            'cms_version' => Version::getVersion(),
+            'current_version' => $this->getCurrentVersion(),
+            'domain' => request()->getHttpHost(),
+        ];
+
+        $activationCodes = get_config('theme_activation_codes', []);
+
+        if (isset($activationCodes[Str::snake($this->name)])) {
+            $params['use_token'] = $activationCodes[Str::snake($this->name)]['token'] ?? null;
+        }
+
+        return $params;
     }
 
     protected function getCacheKey(): string
