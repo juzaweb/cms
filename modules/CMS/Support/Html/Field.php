18c9ea6171b603257907e8c8ef56729affd7eaa1
Juzaweb <dangtheanh16@gmail.com>
2022-10-09 10:28:50 +0700

    :+1: Upload url field

--- modules/CMS/Support/Html/Field.php
+++ modules/CMS/Support/Html/Field.php
@@ -128,6 +128,13 @@ class Field
         return view('cms::components.form_images', $options);
     }
 
+    public static function uploadUrl($label, $name, $options = [])
+    {
+        $options = static::mapOptions($label, $name, $options);
+
+        return view('cms::components.form_upload_url', $options);
+    }
+
     public static function fieldByType($data)
     {
         $type = Arr::get($data, 'type');
@@ -185,6 +192,11 @@ class Field
                 Arr::get($data, 'data', [])
             ),
             'checkbox' => static::checkbox(
+                $data['label'],
+                $data['name'],
+                Arr::get($data, 'data', [])
+            ),
+            'upload_url' => static::uploadUrl(
                 $data['label'],
                 $data['name'],
                 Arr::get($data, 'data', [])
