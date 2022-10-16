dc39dac81790f110bd8feaf33c25a74f99b35581
Juzaweb <dangtheanh16@gmail.com>
2022-10-16 12:59:05 +0700

    :bug: Fix config defaults

--- config/filesystems.php
+++ config/filesystems.php
@@ -28,11 +28,43 @@ return [
     |
     */
 
-    'disks' => \Juzaweb\CMS\Facades\Facades::defaultFileSystemDisks()->merge(
-        [
-            // ...
-        ]
-    )->toArray(),
+    'disks' => [
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'throw' => false,
+        ],
+
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_STORAGE_URL', '/storage'),
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        'backup' => [
+            'driver' => 'local',
+            'root' => storage_path('app/backups'),
+        ],
+
+        'tmp' => [
+            'driver' => 'local',
+            'root' => storage_path('app/tmps'),
+        ],
+
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
