4f86c93249d7a70a780512ac327a0baeda2c0bb2
Juzaweb <dangtheanh16@gmail.com>
2022-12-04 13:46:58 +0700

    :+1: Theme editor

--- modules/Backend/resources/assets/mix.js
+++ modules/Backend/resources/assets/mix.js
@@ -19,38 +19,38 @@ mix.styles(
     `${basePublish}/css/code-editor.min.css`
 );
 
-// mix.styles(
-//     [
-//         baseAsset + '/css/widget.css',
-//         baseAsset + '/css/media.css',
-//         baseAsset + '/css/menu.css',
-//         baseAsset + '/css/page.css',
-//         baseAsset + '/css/customs.css',
-//         baseAsset + '/css/theme.css',
-//     ],
-//     `${basePublish}/css/custom.min.css`
-// );
-//
-// mix.combine(
-//     [
-//         baseAsset + '/js/load-ajax.js',
-//         baseAsset + '/js/helpers.js',
-//         baseAsset + '/js/custom-seo.js',
-//         baseAsset + '/js/filemanager.js',
-//         baseAsset + '/js/widget.js',
-//         baseAsset + '/js/media.js',
-//         baseAsset + '/js/menu.js',
-//         baseAsset + '/js/update.js',
-//         baseAsset + '/js/plugin-install.js',
-//         baseAsset + '/js/theme-install.js',
-//         baseAsset + '/js/load-select2.js',
-//         baseAsset + '/js/juzaweb-table.js',
-//         baseAsset + '/js/list-view.js',
-//         baseAsset + '/js/form-ajax.js',
-//         baseAsset + '/js/taxonomy.js',
-//         baseAsset + '/js/customs.js',
-//         baseAsset + '/js/setting.js',
-//         baseAsset + '/js/page.js',
-//     ],
-//     `${basePublish}/js/custom.min.js`
-// );
+mix.styles(
+    [
+        baseAsset + '/css/widget.css',
+        baseAsset + '/css/media.css',
+        baseAsset + '/css/menu.css',
+        baseAsset + '/css/page.css',
+        baseAsset + '/css/customs.css',
+        baseAsset + '/css/theme.css',
+    ],
+    `${basePublish}/css/custom.min.css`
+);
+
+mix.combine(
+    [
+        baseAsset + '/js/load-ajax.js',
+        baseAsset + '/js/helpers.js',
+        baseAsset + '/js/custom-seo.js',
+        baseAsset + '/js/filemanager.js',
+        baseAsset + '/js/widget.js',
+        baseAsset + '/js/media.js',
+        baseAsset + '/js/menu.js',
+        baseAsset + '/js/update.js',
+        baseAsset + '/js/plugin-install.js',
+        baseAsset + '/js/theme-install.js',
+        baseAsset + '/js/load-select2.js',
+        baseAsset + '/js/juzaweb-table.js',
+        baseAsset + '/js/list-view.js',
+        baseAsset + '/js/form-ajax.js',
+        baseAsset + '/js/taxonomy.js',
+        baseAsset + '/js/customs.js',
+        baseAsset + '/js/setting.js',
+        baseAsset + '/js/page.js',
+    ],
+    `${basePublish}/js/custom.min.js`
+);
