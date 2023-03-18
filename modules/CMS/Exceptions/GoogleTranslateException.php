6dad182099344773ebefd27524175924b0c14712
U-DESKTOP-74JMO6N\User <dangtheanh16@gmail.com>
2023-03-18 17:05:11 +0700

    :bug: Fix set/get command data

---
+++ modules/CMS/Exceptions/GoogleTranslateException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * JUZAWEB CMS - Laravel CMS for Your Project
+ *
+ * @package    juzaweb/juzacms
+ * @author     The Anh Dang
+ * @link       https://juzaweb.com
+ * @license    GNU V2
+ */
+
+namespace Juzaweb\CMS\Exceptions;
+
+use Exception;
+
+class GoogleTranslateException extends Exception
+{
+
+}
