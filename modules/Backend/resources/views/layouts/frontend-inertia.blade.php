f2cad6f5063bfd5ca8d5e3e4520ccea07ecc2d67
Juzaweb <dangtheanh16@gmail.com>
2023-07-16 19:23:44 +0700

    :+1: SSR server

--- modules/Backend/resources/views/layouts/frontend-inertia.blade.php
+++ modules/Backend/resources/views/layouts/frontend-inertia.blade.php
@@ -14,7 +14,45 @@
 
     @do_action('theme.header')
 
+    @viteReactRefresh
+    @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+
     @inertiaHead
+
+    <script>
+        let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+        updateMode()
+        darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
+        window.addEventListener('storage', updateModeWithoutTransitions)
+
+        function updateMode() {
+            let isSystemDarkMode = darkModeMediaQuery.matches
+            let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
+
+            if (isDarkMode) {
+                document.documentElement.classList.add('dark')
+            } else {
+                document.documentElement.classList.remove('dark')
+            }
+
+            if (isDarkMode === isSystemDarkMode) {
+                delete window.localStorage.isDarkMode
+            }
+        }
+
+        function disableTransitionsTemporarily() {
+            document.documentElement.classList.add('[&_*]:!transition-none')
+            window.setTimeout(() => {
+                document.documentElement.classList.remove('[&_*]:!transition-none')
+            }, 0)
+        }
+
+        function updateModeWithoutTransitions() {
+            disableTransitionsTemporarily()
+            updateMode()
+        }
+    </script>
 </head>
 
 <body class="{{ $bodyClass ?? '' }}">
