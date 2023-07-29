c772315c7bcf3680ac76dda30245ea84a49435a4
Juzaweb <dangtheanh16@gmail.com>
2023-07-29 15:18:26 +0700

    :+1: Pagination inertia render

---
+++ themes/default-inertia/views/components/pagination.tsx
@@ -0,0 +1,19 @@
+import {Link} from "@inertiajs/react";
+
+export default function Pagination({ data }) {
+    return (
+        data.meta.last_page > 1 && (
+            data.meta.links.map(
+                (page, index) => (
+                    <Link
+                        key={index}
+                        href={page.url}
+                        className={page.active ? 'active' : ''}
+                    >
+                        {page.label}
+                    </Link>
+                )
+            )
+        )
+    );
+}
