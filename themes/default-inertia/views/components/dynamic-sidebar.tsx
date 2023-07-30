7d84c0498873687c0f6b10aec5cf6483e59bb8ab
Juzaweb <dangtheanh16@gmail.com>
2023-07-30 19:18:53 +0700

    :+1: Render sidebar

--- themes/default-inertia/views/components/dynamic-sidebar.tsx
+++ themes/default-inertia/views/components/dynamic-sidebar.tsx
@@ -1,15 +1,17 @@
 import {getSidebar} from "@/helpers/fetch";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Show from "../widgets/banner/show";
 
 export default function DynamicSidebar() {
     const [sidebar, setSidebar] = useState<{config: Array<any>}>();
 
-    getSidebar('sidebar').then(
-        (res) => {
-            setSidebar(res.data);
-        }
-    );
+    useEffect(() => {
+        getSidebar('sidebar').then(
+            (res) => {
+                setSidebar(res.data);
+            }
+        );
+    }, [])
 
     return sidebar && sidebar.config && sidebar.config.map((item: any) => {
         switch (item.widget) {
