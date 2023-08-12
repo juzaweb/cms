9e02f259a5f5c1ae3d7059c18ef061b852bfaca6
Juzaweb <dangtheanh16@gmail.com>
2023-08-12 18:32:04 +0700

    :+1: Make theme page template UI

--- modules/DevTool/Http/Controllers/Themes/TemplateController.php
+++ modules/DevTool/Http/Controllers/Themes/TemplateController.php
@@ -11,6 +11,11 @@
 namespace Juzaweb\DevTool\Http\Controllers\Themes;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Inertia\Response;
 use Juzaweb\CMS\Contracts\LocalThemeRepositoryContract;
 use Juzaweb\CMS\Interfaces\Theme\ThemeInterface;
@@ -35,6 +40,59 @@ class TemplateController extends Controller
             'cms::backend.dev-tool.theme.template.index',
             compact('settings', 'configs', 'title', 'theme')
         );
+    }
+
+    public function update(Request $request, string $themeName): JsonResponse|RedirectResponse
+    {
+        $theme = $this->themeRepository->findOrFail($themeName);
+
+        $register = $this->getThemeRegister($theme);
+
+        $settings = $this->collectInputData($request->input('settings', []));
+
+        // Merge current data
+        foreach ($settings as $key => $item) {
+            $register['templates'][$key] = array_merge($register['templates'][$key] ?? [], $item);
+        }
+
+        // Delete unused
+        foreach ($register['templates'] as $key => $item) {
+            if (!isset($settings[$key])) {
+                unset($register['templates'][$key]);
+            }
+        }
+
+        File::put(
+            $theme->getPath('register.json'),
+            json_encode($register, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT)
+        );
+
+        return $this->success('Theme Settings updated.');
+    }
+
+    protected function collectInputData(array $settings): array
+    {
+        return collect($settings)
+            ->mapWithKeys(
+                function ($item) {
+                    $name = Str::slug($item['name'], '_');
+                    unset($item['name']);
+
+                    if (isset($item['blocks'])) {
+                        $item['blocks'] = collect($item['blocks'])->mapWithKeys(
+                            function ($item) {
+                                $key = Str::slug($item['name'], '_');
+                                unset($item['name']);
+                                return [$key => $item];
+                            }
+                        )->toArray();
+                    }
+
+                    return [$name => $item];
+                }
+            )
+            ->filter(fn ($item, $key) => !empty($key))
+            ->toArray();
     }
 
     protected function getSettingFields(ThemeInterface $theme): array
