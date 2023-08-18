94ef5482a11d40a090239cd9a0a0ef6e6172d7c2
Juzaweb <dangtheanh16@gmail.com>
2023-08-18 20:14:16 +0700

    :bug: Fix assets url

--- modules/Frontend/routes/assets.php
+++ modules/Frontend/routes/assets.php
@@ -13,18 +13,20 @@ use Juzaweb\Frontend\Http\Controllers\AssetController;
 //Route::get('jw-styles/{lang}/langs.js', [AssetController::class, 'languageScript'])
 //    ->where('lang', '[a-z]+');
 
+$pathRegex = '[0-9a-zA-Z\/\-\.\_]+';
+
 Route::get('jw-styles/themes/{theme}/{path}', [AssetController::class, 'assetTheme'])
     ->where('theme', '[0-9a-z]+')
-    ->where('path', '[0-9a-zA-Z\/\-\.]+');
+    ->where('path', $pathRegex);
 
 Route::get('jw-styles/plugins/{vendor}/{plugin}/{path}', [AssetController::class, 'assetPlugin'])
     ->where('vendor', '[0-9a-z]+')
     ->where('plugin', '[0-9a-z]+')
-    ->where('path', '[0-9a-zA-Z\/\-\.]+');
+    ->where('path', $pathRegex);
 
 Route::get('storage/{path}', [AssetController::class, 'assetStorage'])
-    ->where('path', '[0-9a-zA-Z\/\-\.]+');
+    ->where('path', $pathRegex);
 
 Route::get('jw-styles/images/{method}/{size}/{path}', [AssetController::class, 'proxyImage'])
     ->where('size', '([0-9auto]+)x([0-9auto]+)')
-    ->where('path', '[0-9a-zA-Z\/\-\.]+');
+    ->where('path', $pathRegex);
