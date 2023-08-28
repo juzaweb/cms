355728cd2859812a717eb3d8d8670cc80d1e7727
Juzaweb <dangtheanh16@gmail.com>
2023-08-28 22:04:58 +0700

    :truck: Move modules folder

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../vendor/juzaweb/modules/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
