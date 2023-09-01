19196d1efc44c3e5177a8d9618d531f068f6c596
Juzaweb <dangtheanh16@gmail.com>
2023-09-01 20:07:00 +0700

    :+1: Stats builder

--- public/jw-styles/juzaweb/build/assets/index-b551d865.js
+++
@@ -1,1 +0,0 @@
-import{j as e,F as r}from"./app-32f004ae.js";function t(){return e(r,{children:e("h1",{children:"Welcome"})})}export{t as default};
--- public/jw-styles/juzaweb/build/assets/success-button-2a25f5b9.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-32f004ae.js";import{B as t}from"./button-ca2430d2.js";function u(o){return s(t,{...o,color:"success"})}export{u as S};
