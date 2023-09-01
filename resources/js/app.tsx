c8a78d809b289352cd3cdc992de96973c1c59af7
Juzaweb <dangtheanh16@gmail.com>
2023-09-01 12:53:12 +0700

    :+1: Import js for old js

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,11 +2,30 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import axios from "axios";
+// For old js
+import jQuery from '@types/jquery';
+import Popper from "popper.js";
+import $ from "jquery";
+import 'bootstrap';
+import Chart from 'chart.js/auto';
 
 axios.interceptors.request.use((config) => {
     config.headers['Content-Type'] = 'application/json';
     return config;
 });
+
+declare global {
+    interface Window {
+        jQuery: typeof jQuery;
+        $: typeof jQuery;
+        Chart: typeof Chart;
+        Popper: typeof Popper;
+    }
+}
+
+window.$ = window.jQuery = $;
+window.Popper = Popper;
+window.Chart = Chart;
 
 createInertiaApp({
     title: (title) => title,
