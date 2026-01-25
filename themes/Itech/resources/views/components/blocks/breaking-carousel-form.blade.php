5ec02f3c6c9e4c89616a9a1f0d69608ed692f39e
Juzaweb <47020363+juzaweb@users.noreply.github.com>
2026-01-25 11:13:39 +0000

    Remove unused components and routes from Itech theme
    - Deleted featured posts, hot posts, and advertisement components.
    - Removed language selector and sidebar components.
    - Eliminated post item and most popular widgets.
    - Cleared out profile and notification views.
    - Removed search results and settings pages.
    - Deleted all related routes from web and admin route files.
    - Cleaned up theme.json to remove theme metadata.

--- themes/Itech/resources/views/components/blocks/breaking-carousel-form.blade.php
+++
@@ -1,8 +0,0 @@
-{{ Field::select(__('itech::translation.post_type'), "{$name}[type]", ['value' => $data['type'] ?? 'recent'])->dropDownList(
-    [
-        'recent' => __('itech::translation.recent_posts'),
-        'random' => __('itech::translation.random_posts'),
-    ],
-) }}
-
-{{ Field::text(__('itech::translation.limit'), "{$name}[limit]", ['value' => $data['limit'] ?? '6', 'type' => 'number']) }}
--- themes/Itech/resources/views/components/widgets/tags/form.blade.php
+++
@@ -1,1 +0,0 @@
-{{ Field::text(__('itech::translation.label'), "content[{$data['key']}][label]", ['value' => data_get($data, 'label')]) }}
