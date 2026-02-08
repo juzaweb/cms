e7b1a0fc71a732d7f741b6edbb2b00c9dd73015d
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-02-08 09:55:11 +0700

    feat(core): Add Juzaweb core initialization logic and update default environment variables in example configuration.

--- index.php
+++ index.php
@@ -4,9 +4,15 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+const JW_BASE_PATH = __DIR__ . '/..';
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
+}
+
+if (file_exists(__DIR__.'/vendor/juzaweb/core/helpers/before-init.php')) {
+    require __DIR__.'/vendor/juzaweb/core/helpers/before-init.php';
 }
 
 // Register the Composer autoloader...
