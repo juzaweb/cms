d349e98cf4026bcff48b3418af1d7d89582f7eb6
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-02-11 16:02:47 +0700

    feat: enable Juzaweb core, translations, and permission service providers by uncommenting their registrations.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    // \Juzaweb\Modules\Core\Providers\CoreServiceProvider::class,
-    // \Juzaweb\Modules\Core\Translations\TranslationsServiceProvider::class,
-    // \Juzaweb\Modules\Core\Permissions\PermissionServiceProvider::class,
-    \App\Providers\AppServiceProvider::class,
+    \Juzaweb\Modules\Core\Providers\CoreServiceProvider::class,
+    \Juzaweb\Modules\Core\Translations\TranslationsServiceProvider::class,
+    \Juzaweb\Modules\Core\Permissions\PermissionServiceProvider::class,
     \Juzaweb\Modules\Admin\Providers\AdminServiceProvider::class,
+    \App\Providers\AppServiceProvider::class,
 ];
