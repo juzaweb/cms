241d36b7b2d87a044d097ddba6d812f0237de6f8
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-02-11 15:59:13 +0700

    feat: register new AppServiceProvider and comment out core module providers in bootstrap.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    \Juzaweb\Modules\Core\Providers\CoreServiceProvider::class,
-    \Juzaweb\Modules\Core\Translations\TranslationsServiceProvider::class,
-    \Juzaweb\Modules\Core\Permissions\PermissionServiceProvider::class,
+    // \Juzaweb\Modules\Core\Providers\CoreServiceProvider::class,
+    // \Juzaweb\Modules\Core\Translations\TranslationsServiceProvider::class,
+    // \Juzaweb\Modules\Core\Permissions\PermissionServiceProvider::class,
+    \App\Providers\AppServiceProvider::class,
     \Juzaweb\Modules\Admin\Providers\AdminServiceProvider::class,
 ];
