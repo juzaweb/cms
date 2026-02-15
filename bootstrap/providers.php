195e461c2c73e2b6f3ad9cbe3b0a62d1c91280ad
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-02-15 10:45:31 +0700

    feat(ads-manager): remove ads-manager module and update related dependencies.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,6 @@
 
 return [
     \Juzaweb\Modules\Core\Providers\CoreServiceProvider::class,
-    \Juzaweb\Modules\Core\Translations\TranslationsServiceProvider::class,
-    \Juzaweb\Modules\Core\Permissions\PermissionServiceProvider::class,
     \Juzaweb\Modules\Admin\Providers\AdminServiceProvider::class,
     \App\Providers\AppServiceProvider::class,
 ];
