7170f110701bee14643f9b119dfe65047af20332
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-02-15 11:09:48 +0700

    chore(theme): remove iTech theme and update build configuration and dependencies.

--- themes/itech/src/resources/views/components/widgets/most-popular/form.blade.php
+++
@@ -1,4 +0,0 @@
-{{ Field::text(__('itech::translation.label'), "content[{$data['key']}][label]", ['value' => data_get($data, 'label')]) }}
-
-{{ Field::text(__('itech::translation.number_of_posts_to_show'), "content[{$data['key']}][limit]", ['value' => data_get($data, 'limit', 5), 'min' => 1, 'type' => 'number']) }}
-
