ccf59713f13f7c09972e5eeef77735d369065f80
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-02-15 11:07:40 +0700

    feat(core): register Translations and Permission service providers and update composer dependencies.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,8 @@
 
 return [
     \Juzaweb\Modules\Core\Providers\CoreServiceProvider::class,
+    \Juzaweb\Modules\Core\Translations\TranslationsServiceProvider::class,
+    \Juzaweb\Modules\Core\Permissions\PermissionServiceProvider::class,
     \Juzaweb\Modules\Admin\Providers\AdminServiceProvider::class,
     \App\Providers\AppServiceProvider::class,
 ];
