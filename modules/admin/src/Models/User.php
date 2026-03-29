ae025bddc51bd9c7848315bf4335beac1cc76f9a
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-03-29 11:24:31 +0700

    chore(cleanup): remove root configuration files and update User model

--- modules/admin/src/Models/User.php
+++ modules/admin/src/Models/User.php
@@ -3,9 +3,10 @@
 namespace Juzaweb\Modules\Admin\Models;
 
 use Juzaweb\Modules\Core\Models\User as BaseUser;
+use Juzaweb\Modules\Core\Traits\HasPassportPasswordGrant;
 use Laravel\Passport\HasApiTokens;
 
 class User extends BaseUser
 {
-    use HasApiTokens;
+    use HasApiTokens, HasPassportPasswordGrant;
 }
