09fdf1a9eb0a62953267fc785b6402ddfe9a6787
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-04-05 14:07:10 +0700

    refactor(models): update Client model docblock and import Scope class

--- modules/admin/src/Models/Client.php
+++ modules/admin/src/Models/Client.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * JUZAWEB CMS - Laravel CMS for Your Project
  *
- * @package    juzaweb/cms
  * @author     The Anh Dang
+ *
  * @link       https://cms.juzaweb.com
+ *
  * @license    GNU V2
  */
 
@@ -12,13 +14,14 @@ namespace Juzaweb\Modules\Admin\Models;
 
 use Illuminate\Contracts\Auth\Authenticatable;
 use Laravel\Passport\Client as BaseClient;
+use Laravel\Passport\Scope;
 
 class Client extends BaseClient
 {
     /**
      * Determine if the client should skip the authorization prompt.
      *
-     * @param  \Laravel\Passport\Scope[]  $scopes
+     * @param  Scope[]  $scopes
      */
     public function skipsAuthorization(Authenticatable $user, array $scopes): bool
     {
